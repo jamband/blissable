e3dcb42a0bd2f6f743c6271b2d37a117b905e0cc
Tomoki Morita <tmsongbooks215@gmail.com>
2021-07-24 16:49:22 +0900

    Refactoring postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,27 +1,24 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
 
-const purgecssOptions = {
-  content: [
-    "./src/**/*.html",
-    "./src/**/*.svelte",
-    "./node_modules/bootstrap/js/dist/alert.js",
-    "./node_modules/bootstrap/js/dist/collapse.js",
-    "./node_modules/bootstrap/js/dist/dropdown.js",
-    "./node_modules/bootstrap/js/dist/modal.js",
-    "./node_modules/bootstrap/js/dist/offcanvas.js",
-    "./node_modules/bootstrap/js/dist/toast.js",
-  ],
-  defaultExtractor: (content) => {
-    const _content = content.replace(/(?:^|\s)class:/g, " ");
-    return _content.match(/[\w-/:]+(?<!:)/g) || [];
-  },
-  safelist: [/svelte-/, /ratio-/],
-};
-
-module.exports = {
+module.exports = (context) => ({
   plugins: [
-    ...(process.env.NODE_ENV === "production"
-      ? [purgecss(purgecssOptions)]
-      : []),
+    context.env === "production" &&
+      purgecss({
+        content: [
+          "./node_modules/bootstrap/js/dist/alert.js",
+          "./node_modules/bootstrap/js/dist/collapse.js",
+          "./node_modules/bootstrap/js/dist/dropdown.js",
+          "./node_modules/bootstrap/js/dist/modal.js",
+          "./node_modules/bootstrap/js/dist/offcanvas.js",
+          "./node_modules/bootstrap/js/dist/toast.js",
+          "./src/**/*.html",
+          "./src/**/*.svelte",
+        ],
+        defaultExtractor: (content) => {
+          const _content = content.replace(/(?:^|\s)class:/g, " ");
+          return _content.match(/[\w-/:]+(?<!:)/g) || [];
+        },
+        safelist: [/svelte-/, /ratio-/],
+      }),
   ],
-};
+});
