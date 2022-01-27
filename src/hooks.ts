8cd8397bb77b8d615db8158128c3831c48ed0ce6
Tomoki Morita <tmsongbooks215@gmail.com>
2022-01-27 21:35:09 +0900

    Update dependencies

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,11 +2,11 @@ import { prerendering } from "$app/env";
 import type { Handle } from "@sveltejs/kit";
 import { minify } from "html-minifier-terser";
 
-export const handle: Handle = async ({ request, resolve }) => {
-  const response = await resolve(request);
+export const handle: Handle = async ({ event, resolve }) => {
+  const response = await resolve(event);
 
-  if (prerendering && response.headers["content-type"] === "text/html") {
-    response.body = await minify(response.body as string, {
+  if (prerendering && response.headers.get("content-type") === "text/html") {
+    const body = await minify(await response.text(), {
       collapseBooleanAttributes: true,
       collapseWhitespace: true,
       conservativeCollapse: true,
@@ -21,6 +21,11 @@ export const handle: Handle = async ({ request, resolve }) => {
       removeStyleLinkTypeAttributes: true,
       sortAttributes: true,
       sortClassName: true,
+    });
+
+    return new Response(body, {
+      status: response.status,
+      headers: response.headers,
     });
   }
 
